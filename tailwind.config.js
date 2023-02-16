3f6b5c55be641e4b340c1ed73f89a4ac062e67ab
André Echevarria <echevarriandre@gmail.com>
2023-02-16 14:57:05 +0000

    chore: add serif font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        libre: "'Libre Baskerville', serif",
+      },
+    },
   },
   plugins: [],
 };
